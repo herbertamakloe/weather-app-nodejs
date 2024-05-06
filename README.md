4132074d623cea29b6d1943292aaea72611e57b5
Herbert Tamakloe <herbertamakloe@Herberts-MacBook-Pro.local>
2024-05-06 00:16:48 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Weather App With NodeJS
+
+This is a simple weather application built using Node.js, Express.js, EJS, and Vanilla JavaScript. The app allows users to view the current weather conditions based on their location or by entering a specific city.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/herbertamakloe/weather-app-nodejs.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd weather-app
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+To start the server, run the following command:
+
+```bash
+npm start
+```
+
+The server will start listening on port 3000 by default.
+
+Once the server is running, you can access the weather app in your web browser by visiting [http://localhost:3000](http://localhost:3000).
+
+## Features
+
+- **Automatic Location Detection**: The app automatically detects the user's location based on their IP address and displays the current weather conditions.
+- **Search by City**: Users can enter a specific city name to view the weather conditions for that location.
+- **Responsive Design**: The app is designed to be responsive and works well on both desktop and mobile devices.
+
+## Technologies Used
+
+- **Node.js**: A JavaScript runtime environment used for building server-side applications.
+- **Express.js**: A web application framework for Node.js used for building web applications and APIs.
+- **EJS (Embedded JavaScript)**: A simple templating language that lets you generate HTML markup with plain JavaScript.
+- **Vanilla JavaScript**: Used for client-side scripting to enhance user interactions and dynamically update the UI.
+- **Axios**: A promise-based HTTP client for making HTTP requests from Node.js.
+
+## File Structure
+
+- **index.js**: The main server file responsible for handling routes, rendering EJS templates, and interacting with the OpenWeatherMap API.
+- **views/index.ejs**: The EJS template file containing the HTML structure of the weather app.
+- **public/css/main.css**: The CSS file containing the styles for the weather app UI.
+- **public/images**: The images folder containing the images used for the weather app UI. All the images were designed by me.
+
+## Acknowledgments
+
+- Weather data is retrieved from the [OpenWeatherMap API](https://openweathermap.org/api).
+- Location data is retrieved from the [IPGeolocation API](https://ipgeolocation.io/).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+By [Herbert Tamakloe](https://github.com/herbertamakloe)
---
+++ index.js
@@ -0,0 +1,86 @@
+//1. Import express, body-parser, axios
+
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+//2. Call the express function as a constant with the variable "app"
+const app = express();
+const port = 3000;
+
+//3. Use the public folder for static files
+app.use(express.static("public"));
+//4. Use body-parser for collecting data from form
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const weatherApiKey = "04f1bb004f9171d290163e6756d7c3a2";
+const locationApiKey = "56a3f66afaaa4bce96d53e1985d37830";
+
+//5. Render homepage file to user when they request for homepage
+app.get("/", async (req, res) => {
+  try {
+    // Find out user country/city
+    const locationAPI = await axios.get(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${locationApiKey}`
+    );
+    const userCurrentCity = locationAPI.data.city;
+
+    const weatherAPI = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${userCurrentCity}&units=metric&appid=${weatherApiKey}`
+    );
+
+    const weatherResult = weatherAPI.data;
+    res.render("index.ejs", {
+      weatherDescription: weatherResult.weather[0].description,
+      weatherTemp: Math.round(weatherResult.main.temp),
+      feelsLike: Math.round(weatherResult.main.feels_like),
+      humidity: Math.round(weatherResult.main.humidity),
+      wind: Math.round(weatherResult.wind.speed),
+      location: weatherResult.name,
+      icon: weatherResult.weather[0].icon,
+    });
+  } catch (error) {
+    res.render("index.ejs", {
+      weatherDescription: JSON.stringify(error.response.data.message),
+      weatherTemp: 0,
+      feelsLike: 0,
+      humidity: 0,
+      wind: 0,
+    });
+  }
+});
+
+//6. Handle post request for when the user enters location to know the weather there
+app.post("/", async (req, res) => {
+  let inputtedCity = req.body.city;
+
+  try {
+    const weatherAPI = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputtedCity}&units=metric&appid=${weatherApiKey}`
+    );
+
+    const weatherResult = weatherAPI.data;
+    res.render("index.ejs", {
+      weatherDescription: weatherResult.weather[0].description,
+      weatherTemp: Math.round(weatherResult.main.temp),
+      feelsLike: Math.round(weatherResult.main.feels_like),
+      humidity: Math.round(weatherResult.main.humidity),
+      wind: Math.round(weatherResult.wind.speed),
+      location: weatherResult.name,
+      icon: weatherResult.weather[0].icon,
+    });
+  } catch (error) {
+    res.render("index.ejs", {
+      weatherDescription: JSON.stringify(error.response.data.message),
+      weatherTemp: 0,
+      feelsLike: 0,
+      humidity: 0,
+      wind: 0,
+    });
+  }
+});
+
+//7. Listen for the port and start server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
